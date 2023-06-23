036172beba173ca19fb34e61752b5bbbd5eccdc3
Ryotaro Hada <67140275+ryotarohada@users.noreply.github.com>
2023-06-23 15:19:00 +0900

    リポジトリ削除コードを記述

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # github-local
 GitHub APIをローカルで操作するためのリポジトリ
+
+## 参考資料
+https://medium.com/@yonedac/github-%E3%81%AE%E3%83%AA%E3%83%9D%E3%82%B8%E3%83%88%E3%83%AA%E3%82%92%E4%B8%80%E6%8B%AC%E3%81%A7%E5%89%8A%E9%99%A4%E3%81%99%E3%82%8B-70222150c7bf
+
+## Usage
+
+### delete.py
+.env ファイルを作成して GITHUB_USER と GITHUB_TOKEN を記述
+tokenはプライベートリポジトリへのアクセスとリポジトリ削除の権限を許可する
--- src/delete.py
+++ src/delete.py
@@ -1,1 +1,19 @@
-print("hello")
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GITHUB_USER = os.getenv("GITHUB_USER")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+repos = [
+    # Write the name of the repository you want to delete.
+]
+url = f"https://api.github.com/repos/{GITHUB_USER}/"
+headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+
+for repo in repos:
+    r = requests.delete(url+repo, headers=headers)
+    if r.status_code == 204:
+        print(f"delete succeeded! {repo}")
